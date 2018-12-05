f0e859389c857e8eb32372c91dee4d2934015efe
Galen Elias <gelias@microsoft.com>
2018-12-05 09:09:45 -0800

    Cleanup day 5 further

--- src/Day5/mod.rs
+++ src/Day5/mod.rs
@@ -1,36 +1,25 @@
-use itertools::Itertools;
-
-fn fully_reduce(mut input: Vec<char>) -> Vec<char> {
-	loop {
-		let mut reduced = false;
-
-		for i in (0..input.len()-1).rev() {
-			if input[i].eq_ignore_ascii_case(&input[i+1]) && input[i] != input[i+1] {
-				input.remove(i);
-				input.remove(i);
-				reduced = true;
-			}
-		}
-
-		if !reduced {
-			return input;
+fn reduce(mut input: Vec<char>) -> Vec<char> {
+	for i in (0..input.len()).rev() {
+		if i < input.len() - 1 && input[i].eq_ignore_ascii_case(&input[i+1]) && input[i] != input[i+1] {
+			input.remove(i);
+			input.remove(i);
 		}
 	}
+	return input;
 }
 
 pub fn solve(inputs : Vec<String>) {
-	let input = inputs[0].chars().collect_vec();
+	let input : Vec<char> = inputs[0].chars().collect();
 
-	let input = fully_reduce(input.clone());
+	let input = reduce(input.clone());
 	println!("Part 1: {} ", input.len());
 
 	// Start part 2 with the already reduced part 1 result to save redundant processing in part2
 	let mut min_len = input.len();
 	for i in 0..26 {
 		let ch = (('a' as u8) + i) as char; // Character to remove
-
-		let filtered = input.iter().cloned().filter(|&c| !c.eq_ignore_ascii_case(&ch)).collect_vec();
-		let reduced = fully_reduce(filtered);
+		let filtered : Vec<char> = input.iter().filter(|&c| !c.eq_ignore_ascii_case(&ch)).cloned().collect();
+		let reduced = reduce(filtered);
 		min_len = std::cmp::min(min_len, reduced.len());
 	}
 
