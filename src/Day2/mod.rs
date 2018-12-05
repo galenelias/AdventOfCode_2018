1a7cbd6eba86df9c1b78a750a388f1dd99404d70
Galen Elias <gelias@gmail.com>
2018-12-04 22:34:17 -0800

    Cleanup day 2 slightly

--- src/Day2/mod.rs
+++ src/Day2/mod.rs
@@ -1,9 +1,11 @@
 use itertools::Itertools;
-use std::collections::HashMap;
 
 // Function to see if two collection of chars don't match on a certain number of characters, and returns the string if so, None otherwise
 fn check_diff_by(a: &Vec<char>, b: &Vec<char>, diff_amt: usize) -> Option<String> {
-	let same_chars = a.iter().zip_eq(b.iter()).filter(|(&ca, &cb)| ca == cb).map(|(&c, _)| c).collect_vec();
+	let same_chars = a.iter().zip_eq(b.iter())
+		.filter(|(&ca, &cb)| ca == cb)
+		.map(|(&c, _)| c)
+		.collect_vec();
 	if same_chars.len() == a.len() - diff_amt {
 		Some(same_chars.iter().collect::<String>())
 	} else {
@@ -14,17 +16,17 @@ fn check_diff_by(a: &Vec<char>, b: &Vec<char>, diff_amt: usize) -> Option<String
 pub fn solve(inputs : Vec<String>) {
 	let inputs = inputs.iter().map(|line| line.chars().collect_vec()).collect_vec();
 
-	let freqs = inputs.iter()
+	let all_freqs = inputs.iter()
 		.map(|line| {
-			let mut frequencies = HashMap::new();
-			for c in line {
-				*(frequencies.entry(c).or_insert(0)) += 1;
+			let mut frequencies = [0u32; 256];
+			for &c in line {
+				frequencies[c as usize] += 1;
 			}
 			frequencies
 		}).collect_vec();
 
-	let twos = freqs.iter().filter(|freq_map| freq_map.values().any(|&v| v == 2)).count();
-	let threes = freqs.iter().filter(|freq_map| freq_map.values().any(|&v| v == 3)).count();
+	let twos = all_freqs.iter().filter(|freqs| freqs.iter().any(|&v| v == 2)).count();
+	let threes = all_freqs.iter().filter(|freqs| freqs.iter().any(|&v| v == 3)).count();
 	println!("Part 1: {}", twos * threes);
 
 	let part2 = inputs.iter()
