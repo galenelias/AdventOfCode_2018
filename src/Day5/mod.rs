0d1bc054beac83df93db358a8d2ab307c31d2fb2
Galen Elias <gelias@microsoft.com>
2018-12-05 19:07:06 -0800

    Refactor day 5 a bit more
    More functional!

--- src/Day5/mod.rs
+++ src/Day5/mod.rs
@@ -1,6 +1,6 @@
 fn reduce(mut input: Vec<char>) -> Vec<char> {
 	for i in (0..input.len()).rev() {
-		if i < input.len() - 1 && input[i].eq_ignore_ascii_case(&input[i+1]) && input[i] != input[i+1] {
+		if i < input.len() - 1 && input[i] != input[i+1] && input[i].eq_ignore_ascii_case(&input[i+1]) {
 			input.remove(i);
 			input.remove(i);
 		}
@@ -11,17 +11,15 @@ fn reduce(mut input: Vec<char>) -> Vec<char> {
 pub fn solve(inputs : Vec<String>) {
 	let input : Vec<char> = inputs[0].chars().collect();
 
-	let input = reduce(input.clone());
+	let input = reduce(input);
 	println!("Part 1: {} ", input.len());
 
 	// Start part 2 with the already reduced part 1 result to save redundant processing in part2
-	let mut min_len = input.len();
-	for i in 0..26 {
+	let part2 = (0..26).map(|i| {
 		let ch = (('a' as u8) + i) as char; // Character to remove
 		let filtered : Vec<char> = input.iter().filter(|&c| !c.eq_ignore_ascii_case(&ch)).cloned().collect();
-		let reduced = reduce(filtered);
-		min_len = std::cmp::min(min_len, reduced.len());
-	}
+		reduce(filtered).len()
+	}).min().unwrap();
 
-	println!("Part 2: {}", min_len);
+	println!("Part 2: {}", part2);
 }
