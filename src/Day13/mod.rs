b51dcbdb49c88f0aaedc82ee74cc023263566dc4
Galen Elias <gelias@microsoft.com>
2018-12-13 14:11:51 -0800

    Day 13, part 2 in Rust via RefCell

--- src/Day13/mod.rs
+++ src/Day13/mod.rs
@@ -1,17 +1,18 @@
-use std::collections::HashSet;
+use std::cell::RefCell;
 use itertools::Itertools;
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(Debug, Clone)]
 enum Direction {
-	Left,Up,Down,Right
+	Left,Up,Right,Down
 }
 
-#[derive(PartialEq, Eq, Debug)]
+#[derive(Debug, Clone)]
 struct Cart {
 	r: usize,
 	c: usize,
 	direction: Direction,
-	turn_preference: usize, // left, straight, right	
+	turn_preference: usize, // left, straight, right
+	is_alive: bool,
 }
 
 fn turn_left(dir: &Direction) -> Direction {
@@ -24,6 +25,7 @@ fn turn_left(dir: &Direction) -> Direction {
 }
 
 fn turn_right(dir: &Direction) -> Direction {
+	// (dir + 1) % 4
 	match dir {
 		Direction::Up => Direction::Right,
 		Direction::Right => Direction::Down,
@@ -32,71 +34,16 @@ fn turn_right(dir: &Direction) -> Direction {
 	}
 }
 
-
-// fn go_straight(dir: &Direction) -> Direction {
-// 	dir.clone()
-// }
-
-
-pub fn solve(inputs : Vec<String>) {
-	// let inputs = inputs.iter().map(|line| line.parse::<i32>().unwrap()).collect_vec();
-	let mut grid : Vec<Vec<char>> = inputs.iter().map(|line| line.chars().collect_vec()).collect_vec();
-
-	let mut carts = Vec::new();
-	println!("{:?}", grid[0]);
-
-	for r in 0..grid.len() {
-		for c in 0..grid[r].len() {
-			let ch = grid[r][c];
-			if ch == '<' {
-				carts.push( Cart{ r: r, c: c, direction: Direction::Left, turn_preference: 0});
-				grid[r][c] = '-';
-			} else if ch == '>' {
-				carts.push( Cart{ r: r, c: c, direction: Direction::Right, turn_preference: 0});
-				grid[r][c] = '-';
-			} else if ch == '^' {
-				carts.push( Cart{ r: r, c: c, direction: Direction::Up, turn_preference: 0});
-				grid[r][c] = '|';
-			} else if ch == 'v' {
-				carts.push( Cart{ r: r, c: c, direction: Direction::Down, turn_preference: 0});
-				grid[r][c] = '|';
-			}
-		}
-	}
-
-	for iteration in 0.. {
-		println!("Iteration {}: {}", iteration, carts.len());
-
-		// println!("{:?}", carts);
-
-		let mut positions = HashSet::new();
-		// for cart in &carts {
-		// 	if !positions.insert((cart.r, cart.c)) {
-		// 		println!("Collision at {} ({}, {})", iteration, cart.c, cart.r);
-		// 		return;
-		// 	}
-		// }
-		carts.sort_by_key(|cart| (cart.r, cart.c));
+fn sub_solve(grid : &Vec<Vec<char>>, mut carts: Vec<RefCell<Cart>>, stop_on_first_collision: bool) -> Option<(usize, usize)> {
+	for _iteration in 0.. {
+		carts.sort_by_key(|cart| (cart.borrow().r, cart.borrow().c));
 		
-		carts = carts.into_iter().filter(|cart| positions.insert((cart.r, cart.c))).collect_vec();
+		for (i, cart) in carts.iter().enumerate() {
+			let mut cart = cart.borrow_mut();
+			if !cart.is_alive {
+				continue;
+			}
 
-		if carts.len() == 1 {
-			println!("Part 2: {:?}", carts[0]);
-			break;
-		}
-
-		// for r in 0..grid.len() {
-		// 	for c in 0..grid[r].len() {
-		// 		if positions.contains(&(r, c)) {
-		// 			print!("X");
-		// 		} else {
-		// 			print!("{}", grid[r][c]);
-		// 		}
-		// 	}
-		// 	println!("");
-		// }
-
-		for cart in &mut carts {
 			cart.c = match cart.direction {
 				Direction::Up | Direction::Down => cart.c,
 				Direction::Left => cart.c - 1,
@@ -110,13 +57,11 @@ pub fn solve(inputs : Vec<String>) {
 			};
 
 			let ch = grid[cart.r][cart.c];
-			if ch == ' ' {
-				panic!("OH Crap!");
-			} else if ch == '+' {
+			if ch == '+' {
 				if cart.turn_preference == 0 {
 					cart.direction = turn_left(&cart.direction);
 				} else if cart.turn_preference == 1 {
-					//cart.direction = go_straight(&cart.direction);
+					// no-op
 				} else if cart.turn_preference == 2 {
 					cart.direction = turn_right(&cart.direction);
 				} 
@@ -135,20 +80,63 @@ pub fn solve(inputs : Vec<String>) {
 					Direction::Down => Direction::Right,
 					Direction::Right => Direction::Down,
 				};
+			} else if ch == ' ' {
+				panic!("Off the rails!");
+			}
+
+			// Check for collisions
+			for (j, other_cart) in carts.iter().enumerate() {
+				// Can't borrow ourselves, so make sure we don't call .borrow if our indices are the same
+				if i == j {
+					continue;
+				}
+				let mut other_cart = other_cart.borrow_mut();
+				if other_cart.is_alive && cart.r == other_cart.r && cart.c == other_cart.c {
+					if stop_on_first_collision {
+						return Some((cart.c, cart.r));
+					}
+
+					cart.is_alive = false;
+					other_cart.is_alive = false;
+				}
 			}
 		}
 
-		carts = carts.into_iter().filter(|cart| positions.insert((cart.r, cart.c))).collect_vec();
-		if carts.len() == 1 {
-			println!("Part 2!: {:?}", carts[0]);
-			break;
-		}
+		// Remove dead carts
+		carts = carts.into_iter().filter(|cart| cart.borrow().is_alive).collect_vec();
 
-		// for cart in &carts {
-		// 	if !positions.insert((cart.r, cart.c)) {
-		// 		println!("Collision at {} ({}, {})", iteration, cart.c, cart.r);
-		// 		return;
-		// 	}
-		// }
+		if carts.len() == 1 {
+			let last_cart = carts[0].borrow();
+			return Some((last_cart.c, last_cart.r));
+		}
 	}
+
+	None
+}
+
+pub fn solve(inputs : Vec<String>) {
+	let grid : Vec<Vec<char>> = inputs.iter().map(|line| line.chars().collect_vec()).collect_vec();
+	let mut carts = Vec::new();
+
+	for r in 0..grid.len() {
+		for c in 0..grid[r].len() {
+			let ch = grid[r][c];
+			if ch == '<' || ch == '>' || ch == 'v' || ch == '^' {
+				let dir = match ch {
+					'<' => Direction::Left,
+					'>' => Direction::Right,
+					'^' => Direction::Up,
+					'v' => Direction::Down,
+					_ => unreachable!(),
+				};
+				carts.push(RefCell::new(Cart{ r: r, c: c, direction: dir, turn_preference: 0, is_alive: true}));
+			}
+		}
+	}
+
+	let part1 = sub_solve(&grid, carts.clone(), true /*stop_on_first_collision*/);
+	println!("Part 1: {:?}", part1.expect("Part 1 failed"));
+
+	let part2 = sub_solve(&grid, carts, false /*stop_on_first_collision*/);
+	println!("Part 2: {:?}", part2.expect("Part 2 failed"));
 }
