137e65974ed7fb5b7701dff23866f80595e1c9d8
Galen Elias <gelias@microsoft.com>
2018-12-12 17:21:08 -0800

    Cleanup day 12 a bit
    Track used pots as just as HashSet, not a HashMap, since we're only storing values which map to true anyway.

--- src/Day12/mod.rs
+++ src/Day12/mod.rs
@@ -1,34 +1,32 @@
 use itertools::Itertools;
-use std::collections::HashMap;
+use std::collections::{HashSet, HashMap};
 
-fn state_to_string(state: &HashMap<i32, bool>) -> String {
-	let min_index = state.keys().cloned().min().unwrap();
-	let max_index = state.keys().cloned().max().unwrap();
+fn state_to_string(state: &HashSet<i32>) -> String {
+	let min_index = state.iter().cloned().min().unwrap();
+	let max_index = state.iter().cloned().max().unwrap();
 
 	let mut result = String::new();
 	for x in min_index..=max_index {
-		result.push(if *state.get(&x).unwrap_or(&false) { '#' } else { '.' });
+		result.push(if state.contains(&x) { '#' } else { '.' });
 	}
 	result
 }
 
-fn get_total(state: &HashMap<i32, bool>, bias_shift: i64) -> i64 {
+fn get_total(state: &HashSet<i32>, bias_shift: i64) -> i64 {
 	let mut total = 0;
-	for (&k, &v) in state {
-		if v {
-			total += k as i64 + bias_shift;
-		}
+	for &index in state {
+		total += index as i64 + bias_shift;
 	}
 	total
 }
 
 pub fn solve(inputs : Vec<String>) {
 	let initial_state = inputs[0][(inputs[0].find(": ").unwrap() + 2)..].to_string();
-	let mut state : HashMap<i32, bool> = HashMap::new();
+	let mut state : HashSet<i32> = HashSet::new();
 
 	for (i, c) in initial_state.chars().enumerate() {
 		if c == '#' {
-			state.insert(i as i32, true);
+			state.insert(i as i32);
 		}
 	}
 
@@ -41,16 +39,16 @@ pub fn solve(inputs : Vec<String>) {
 
 	let mut state_str = state_to_string(&state);
 	for iter in 0.. {
-		let mut new_state = HashMap::new();
+		let mut new_state = HashSet::new();
 
-		let min_index = state.keys().cloned().min().unwrap();
-		let max_index = state.keys().cloned().max().unwrap();
+		let min_index = state.iter().cloned().min().unwrap();
+		let max_index = state.iter().cloned().max().unwrap();
 
 		for index in (min_index - 2)..=(max_index + 2) {
 			let mut key = String::new();
 
 			for i in -2i32..=2i32 {
-				if *state.get(&(index + i)).unwrap_or(&false) {
+				if state.contains(&(index + i)) {
 					key.push('#');
 				} else {
 					key.push('.');
@@ -60,7 +58,7 @@ pub fn solve(inputs : Vec<String>) {
 			let res = rules.get(&key).unwrap_or(&'.');
 
 			if res == &'#' {
-				new_state.insert(index, true);
+				new_state.insert(index);
 			}
 		}
 
@@ -70,8 +68,6 @@ pub fn solve(inputs : Vec<String>) {
 
 		let new_state_str = state_to_string(&new_state);
 		if state_str == new_state_str {
-			let new_min_index = *new_state.keys().min().unwrap();
-			// println!("Duplicate after {}, shifting by {}", iter, new_min_index - min_index);
 			println!("Part 2: {}", get_total(&state, 50000000000i64 - iter));
 			break;
 		}
