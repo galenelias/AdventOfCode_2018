fae5bb3a18afb927e60fcd65b8b05ad6f340b58c
Galen Elias <gelias@microsoft.com>
2018-12-19 21:58:21 -0500

    Re-write day 15
    The target prioritization code for day 15 was more complex than it
    needed to be, tracking the target entity rather than the target
    movement square.  Re-worked it by making all unreachable values now
    usize::max_value(), and only considering adjacent squares to targets.
    This means we need to be able to realize we should stay put with this
    prioritization.

--- src/Day15/mod.rs
+++ src/Day15/mod.rs
@@ -15,12 +15,13 @@ struct Entity {
 }
 
 // Returns a matrix of distances, for each reachable square, plus the first step in the shortest route to get to that square
-// (needs_movement, distance, pos, first_step_pos)
-fn build_distances(pos: (usize, usize), grid: &Vec<Vec<char>>) -> Vec<Vec<(bool, usize, (usize, usize), (usize, usize))>> {
+// (distance, first_step_pos)
+fn build_distances(pos: (usize, usize), grid: &Vec<Vec<char>>) -> Vec<Vec<(usize, (usize, usize))>> {
 	let mut queue = VecDeque::new();
-
 	let mut visited = HashSet::new();
-	let mut distances = vec![vec![(true, 0, (0, 0), (0, 0)); grid[0].len()]; grid.len()];
+	let mut distances = vec![vec![(usize::max_value(), (0, 0)); grid[0].len()]; grid.len()];
+
+	distances[pos.0][pos.1] = (0, pos);
 
 	// Start navigation, recording the intial step in our results so we can quickly resolve the next step
 	// We need to make sure we start navigation in reading order for the BFS to yield optimal results
@@ -33,32 +34,24 @@ fn build_distances(pos: (usize, usize), grid: &Vec<Vec<char>>) -> Vec<Vec<(bool,
 	while !queue.is_empty() {
 		let (p, d, fs) = queue.pop_front().unwrap();
 		let ch = grid[p.0][p.1];
-		if ch == '#' {
+		if ch == '#' || ch == 'G' || ch == 'E' {
 			continue;
 		}
 		if !visited.insert(p) {
 			continue;
 		}
 
-		let is_entity = ch == 'G' || ch == 'E';
-		distances[p.0][p.1] = (!is_entity || d > 1, d, p, fs);
-
-		// Record distances to other entities, but don't move past them
-		if is_entity {
-			continue;
-		}
-
+		distances[p.0][p.1] = (d, fs);
 		queue.push_back(((p.0 - 1, p.1), d + 1, fs));
-		queue.push_back(((p.0 + 1, p.1), d + 1, fs));
 		queue.push_back(((p.0, p.1 - 1), d + 1, fs));
 		queue.push_back(((p.0, p.1 + 1), d + 1, fs));
+		queue.push_back(((p.0 + 1, p.1), d + 1, fs));
 	}
 	distances
 }
 
 fn sub_solve(inputs : Vec<String>, elf_attack: i32, allow_elf_deaths: bool, verbose: bool) -> Option<(Race, i32, i32)> {
     let mut grid = inputs.iter().map(|line| line.chars().collect_vec()).collect_vec();
-
     let mut entities = Vec::new();
 
     // Pull out all entities
@@ -78,11 +71,6 @@ fn sub_solve(inputs : Vec<String>, elf_attack: i32, allow_elf_deaths: bool, verb
 
 		entities.sort_by_key(|entity| entity.borrow().pos);
 		
-		// for entity in &entities {
-		// 	let entity = entity.borrow();
-		// 	println!("{:?}", entity);
-		// }
-
         let mut no_targets = false;
 
 		if verbose {
@@ -104,15 +92,14 @@ fn sub_solve(inputs : Vec<String>, elf_attack: i32, allow_elf_deaths: bool, verb
 
 			if verbose { println!("Turn {}, Entity {:?}:", iterations, entity); }
 
-			let distances = build_distances(entity.pos, &grid);
+			let distances = build_distances2(entity.pos, &grid);
 
+			// Dist, pos, first_step, health, enemy_pos
+			let mut target : Option<(usize, (usize, usize), (usize, usize), i32, (usize, usize))> = None;
 			let mut target_ent = None;
-			let mut target_val : Option<(i32, (usize, usize))> = None; // (Health, pos), for prioritization
-			let mut target_dist: Option<(bool, usize, (usize, usize), (usize, usize))> = None; // (needs_movement, dist, pos, first_step_pos)
 
 			let no_one_to_fight = !entities.iter().enumerate().any(|(j, other)| i != j && other.borrow().health > 0 && other.borrow().race != entity.race);
 			if no_one_to_fight {
-				// println!("No targets");
 				no_targets = true;
 				continue;
 			}
@@ -124,53 +111,24 @@ fn sub_solve(inputs : Vec<String>, elf_attack: i32, allow_elf_deaths: bool, verb
 				let other = other_rc.borrow();
 				
 				// Ignore if dead, same race, or unreachable
-				let dist = distances[other.pos.0][other.pos.1]; 
-				if other.health <= 0 || entity.race == other.race || dist.1 == 0 {
+				if other.health <= 0 || entity.race == other.race {
 					continue;
 				}
 
-				if verbose { println!("Considering {:?}: {:?}", other, dist); }
-				// if target_dist.is_some() && target_dist.unwrap() < dist {
-				// 	println!("Too far");
-				// 	continue;
-				// }
-				let other_val = (other.health, other.pos);
-				// if target_dist.is_some() && target_dist.unwrap().0 == 1 && dist.0 == 1 &&  {
-				// 	println!("Too low value");
-				// 	continue;
-				// }
+				if verbose { println!("Considering {:?}:", other); }
 
-				// Adjacent target_ent
-				if dist.1 == 1 {
-					if target_dist.is_some() && target_dist.unwrap().0 == false && target_val.unwrap() < other_val {
-						// println!("Too low value");
+				let op = other.pos;
+				for &(oy, ox) in &[(op.0-1, op.1), (op.0, op.1-1), (op.0, op.1+1), (op.0+1, op.1)] {
+					let (dist, first_step) = distances[oy][ox];
+					if dist == usize::max_value() {
 						continue;
 					}
-					assert!(dist.0 == false);
-					target_dist = Some(dist);
-					target_val = Some(other_val);
-					target_ent = Some(other_rc);
-				} else {
-					// for (dy, dx) in &[(-1i32,0), (0,-1i32), (0,1i32), (1i32,0)] {
-					// 	let other_dist = distances[(other.pos.0 as i32 + dy) as usize][(other.pos.1 as i32 + dx) as usize];
-					let op = other.pos;
-					for &(oy, ox) in &[(op.0-1, op.1), (op.0, op.1-1), (op.0, op.1+1), (op.0+1, op.1)] {
-						let other_dist = distances[oy][ox];
-						if grid[oy][ox] != '.' || other_dist.1 == 0 {
-							continue;
-						}
-						assert!(dist.0 == true);
 
-						if target_dist.is_some() {
-							let target_dist = target_dist.unwrap();
-							if target_dist < other_dist {
-								// println!("Too far");
-								continue;
-							}
-						}
+					// Dist, pos, first_step, health, enemy_pos
+					let other_val = (dist, (oy, oy), first_step, other.health, other.pos);
 
-						target_dist = Some(other_dist);
-						target_val = Some(other_val);
+					if target.is_none() || other_val < target.unwrap() {
+						target = Some(other_val);
 						target_ent = Some(other_rc);
 					}
 				}
@@ -181,20 +139,18 @@ fn sub_solve(inputs : Vec<String>, elf_attack: i32, allow_elf_deaths: bool, verb
 			}
 
 			let mut target_ent = target_ent.unwrap().borrow_mut();
-			let target_dist = target_dist.unwrap();
-
-			if verbose { println!("Target: {:?}, {:?}", target_ent, target_dist); }
+			if verbose { println!("Target: {:?}, {:?}", target_ent, target.unwrap()); }
+			let (target_dist, _target_pos, target_first_step, _target_health, _entity_pos) = target.unwrap();
 
 			// If we're more than one square away, our turn is moving, otherwise combat
-			if target_dist.0 {
-				let target_first_step = target_dist.3;
+			if target_dist > 0 {
 				if verbose { println!("Moved from {:?} -> {:?}", entity.pos, target_first_step); }
 				grid[target_first_step.0][target_first_step.1] = grid[entity.pos.0][entity.pos.1];
 				grid[entity.pos.0][entity.pos.1] = '.';
 				entity.pos = target_first_step;
 			}
-
-			if target_dist.1 <= 1 {
+			
+			if target_dist <= 1 {
 				//Combat!
 				if verbose { println!("Attack! {} -> {}", target_ent.health, target_ent.health - 3); }
 				if entity.race == Race::Elf {
@@ -206,8 +162,6 @@ fn sub_solve(inputs : Vec<String>, elf_attack: i32, allow_elf_deaths: bool, verb
 				if target_ent.health <= 0 {
 					if !allow_elf_deaths && target_ent.race == Race::Elf {
 						return None;
-						// println!("Elf death in round {}: {:?}", iterations, target_ent);
-						// return false;
 					}
 					if verbose { println!("{:?} death in round {}: {:?}", target_ent.race, iterations, target_ent); }
 					grid[target_ent.pos.0][target_ent.pos.1] = '.';
@@ -229,7 +183,7 @@ fn sub_solve(inputs : Vec<String>, elf_attack: i32, allow_elf_deaths: bool, verb
 }
 
 pub fn solve(inputs : Vec<String>) {
-	let verbose = true;
+	let verbose = false;
 	// Part 1:
 	let (part1_race, part1_rounds, part1_healths) = sub_solve(inputs.clone(), 3, true /*allow_elf_deaths*/, verbose).unwrap();
 	println!("Part 1: {} ({:?} won in {} rounds with {} health remaining)", part1_rounds * part1_healths, part1_race, part1_rounds, part1_healths);
@@ -241,5 +195,4 @@ pub fn solve(inputs : Vec<String>) {
 			break;
 		}
 	}
-
 }
